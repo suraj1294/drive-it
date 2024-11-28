b497c98fe9ccb1a2e2aa8c553e3a94c02ac526c9
suraj <suraj@funnelstory.io>
2024-11-28 10:39:13 +0530

    chore: add global styles,shadcn config, assets and auth routes

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,6 @@
+import AuthForm from "@/components/auth-form";
+import React from "react";
+
+const SignIn = () => <AuthForm type="sign-in" />;
+
+export default SignIn;
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,6 @@
+import AuthForm from "@/components/auth-form";
+import React from "react";
+
+const SignOut = () => <AuthForm type="sign-up" />;
+
+export default SignOut;
